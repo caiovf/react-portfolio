fb975ec8f708e90ecb0cd2c163cfce3c6c44c97f
Caio Ferreira <ca.victor2@gmail.com>
2024-08-04 23:16:39 -0300

    :recycle: refactor atualização dos dados da api

--- src/components/sections/home/profile/profile.js
+++ src/components/sections/home/profile/profile.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { fetchData } from '../../../../api/api';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import DOMPurify from 'dompurify';
 import { Button } from '../../../button';
 import './profile.scss';
 
@@ -32,7 +33,7 @@ export const Profile = (props) => {
                         <Skeleton height={60} width={230} count={1} />
                     </div>
                     <div className='right-content'>
-                        <Skeleton height={682} width={682} count={1} />
+                        <Skeleton height={682} width={682} count={1} borderRadius="250px" />
                     </div>
                 </div>
             </section>
@@ -45,8 +46,8 @@ export const Profile = (props) => {
         <section className='profile'>
             <div className='container'>
                 <div className='left-content'>
-                    <h1 data-custom-title="banner">{data.title}</h1>
-                    <p>{data.description}</p>
+                    <h1 data-custom-title="banner" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.title) }}></h1>
+                    <p dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.description) }}></p>
                     <Button                        
                         iconSrc="download.svg"
                         iconWidth="24"
