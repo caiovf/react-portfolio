903b98cb6cdcec3c433ce64fa10757ac224e8b88
Caio Ferreira <ca.victor2@gmail.com>
2024-09-29 13:52:11 -0300

    :recycle: Atualizando chamada do env

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,11 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+console.log(process.env);
+console.log(supabaseUrl);
+console.log(supabaseKey);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);        
