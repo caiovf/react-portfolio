b0d8a18033385fc034ee599fc20ed9443439a6ac
Caio Ferreira <ca.victor2@gmail.com>
2026-04-15 23:23:33 -0300

    chore: remove @vercel/react-router dependency and configure custom Vercel build settings

--- react-router.config.js
+++ react-router.config.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 import { index, route } from "@react-router/dev/routes";
-import { vercelPreset } from '@vercel/react-router/vite';
 
 export default {
   // Configuração para SSG (Static Site Generation)
   ssr: true,
-  presets: [vercelPreset()],
   async prerender() {
     const baseURL = 'https://raw.githubusercontent.com/caiovf/react-portfolio/main/api/';
     const routes = ["/", "/about", "/portfolio", "/advancing-skills", "/plugins"];
